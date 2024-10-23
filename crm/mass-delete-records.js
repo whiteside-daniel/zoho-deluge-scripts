2cbe41757505905b8a5045af81dcc4dfd2ccf475
Daniel Whiteside <whiteside.danielj@gmail.com>
2024-10-22 22:52:31 -0400

    Update mass-delete-records.js

--- crm/mass-delete-records.js
+++ crm/mass-delete-records.js
@@ -1,6 +1,9 @@
 //as of June 2024... ~3 min to delete 10,000 records
+//Description: This funtion iterates through up to 10,000 records in a module and deletes them.
+//You should configure it as a standalone and use it wiseley
 //
-//make a double for loop to run 100 times per CF call
+//
+//Step 1 - make a double for loop to run 100 times per CF call
 repeatList = [1,2,3,4,5,6,7,8,9,10];
 secondRepeat = [1,2,3,4,5,6,7,8,9,10];
 for each rep in secondRepeat {
@@ -16,7 +19,7 @@ for each rep in secondRepeat {
 
 		}
 		formattedIdList = idList.toString();
-		//delete 100 records at a time... this is the max for this API
+		//delete 100 records at a time... this is the maximum for this API so we will repeat it 100x
 		recordDelete = invokeurl
 			[
 				url :"https://www.zohoapis.com/crm/v3/Module_Name?ids=" + formattedIdList
