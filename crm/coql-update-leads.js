72280525be22f869c8f2e61a55626b275e87dbb6
Daniel Whiteside <whiteside.danielj@gmail.com>
2025-02-05 21:57:58 -0600

    add coql query

---
+++ crm/coql-update-leads.js
@@ -0,0 +1,19 @@
+string button.getDealTotal(String leadID)
+{
+queryMap = Map();
+queryString = "select SUM(Amount) from Deals where Associated_Lead =" + leadID;
+queryMap.put("select_query",queryString);
+response = invokeurl
+[
+	url :"https://www.zohoapis.com/crm/v7/coql"
+	type :POST
+	parameters:queryMap.toString()
+	connection:"coqlconnection"
+];
+info response;
+sumOfDeals = response.get('data').get(0).get("SUM(Amount)");
+info sumOfDeals;
+updateParams = {"Pending_Deal_Total":sumOfDeals};
+updateResponse = zoho.crm.updateRecord("Leads",leadID,updateParams);
+return "ok";
+}
